d7ba42a26b20e55cef6b62abe8a09fcee293e6da
Brian Ethier <davidbrianethier@gmail.com>
2017-03-08 22:18:28 -0700

    Part 2 of question 4 done

--- 4.rb
+++ 4.rb
@@ -1,9 +1,11 @@
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def parse_input(input)
   line = input.split('-')
   letter_sections = line[0..-2]
 
   sector_and_checksum = line[-1].split('[')
-  sector_id = sector_and_checksum[0]
+  sector_id = sector_and_checksum[0].to_i
   checksum = sector_and_checksum[1][0..-2]
 
   return letter_sections, sector_id, checksum
@@ -36,23 +38,38 @@ def calculate_checksum(letter_map)
   return (letter_count_array[0..4].map {|lc| lc[:letter]}).join('')
 end
 
-
-def main
-  sector_sum = 0
-
-  while(input = gets.chomp) != ''
-    letter_sections, sector_id, checksum = parse_input(input)
-
-    letter_map = create_letter_map(letter_sections)
-
-    actual_checksum = calculate_checksum(letter_map)
-
-    if checksum == actual_checksum
-      sector_sum += sector_id.to_i
+def decypher(letter_sections, sector_id)
+  shift_amount = sector_id % 26
+  
+  letter_sections.each do |section|
+    for i in 0..section.length - 1
+      index = ALPHABET.index(section[i])
+      index += shift_amount
+      index %= 26
+      section[i] = ALPHABET[index]
     end
   end
 
-  puts "Sector sum: #{sector_sum}"
+  return letter_sections.join(' ')
+end
+
+
+def main
+  sentence_sector_array = []
+
+  while(input = gets.chomp) != ''
+    letter_sections, sector_id, checksum = parse_input(input)
+    letter_map = create_letter_map(letter_sections)
+    actual_checksum = calculate_checksum(letter_map)
+
+    if checksum == actual_checksum
+      sentence_sector_array << {sentence: decypher(letter_sections, sector_id), sector: sector_id}
+    end
+  end
+
+  sentence_sector_array.each do |ss|
+    puts "#{ss[:sentence]} -- #{ss[:sector]}"
+  end
 end
 
 main
